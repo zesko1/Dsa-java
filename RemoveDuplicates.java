5d33324248221877e0692d43dcbad10af4285b16
Zesko1 <harsin019@gmail.com>
2026-06-05 13:23:13 +0530

    RemoveDuplicates

---
+++ RemoveDuplicates.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+//Approach - Two pointer approach, one pointer to keep track of the last unique element and another pointer to traverse the array. Whenever we find a new unique element, we move the first pointer and update the value at that position with the new unique element. Finally, we return the length of the unique elements which is the position of the first pointer + 1.
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        int n = nums.length;
+
+        for(int j = 1; j < n; j++)
+        {
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+} 
