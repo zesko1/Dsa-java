139c51b02bc6c315f4e50ca5c1a54637bf9707bc
Zesko1 <harsin019@gmail.com>
2026-07-08 16:59:26 +0530

    Maximum Product Subarray LC-152 added

---
+++ MaxProductSubarra.java
@@ -0,0 +1,30 @@
+//Maximum Product Subarray LC-152
+//TC - O(N)
+//SC - O(1)
+
+class MaxProductSubarray {
+    public int maxProduct(int[] nums) {
+        
+        int prefix = 1;
+        int suffix = 1;
+
+        int ans = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(prefix == 0){
+                prefix = 1;
+            }
+            if(suffix == 0){
+                suffix = 1;
+            }
+
+            prefix *= nums[i];
+
+            suffix *= nums[nums.length - i - 1];
+
+            ans = Math.max(ans, Math.max(prefix, suffix));
+        }
+        return ans;
+    }
+}
